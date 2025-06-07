f22044ccd380f49493c25a93220cb4d5c3301611
dashort <31360544+dashort@users.noreply.github.com>
2025-06-06 19:35:12 -0500

    Clean up repo and remove sensitive data

--- src/process/Home.java
+++ src/process/Home.java
@@ -1,22 +1,21 @@
 package process;
 
-<<<<<<< HEAD
 import java.awt.CardLayout;
 import java.awt.EventQueue;
-=======
-
-import java.awt.CardLayout;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
->>>>>>> bb500479f201c5b0b636cd9283c4f25d780a2c0f
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-<<<<<<< HEAD
-import javax.swing.*;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ * Simple Swing launcher for importing and deleting account data.
+ */
 public class Home {
     private JFrame mainFrame;
     private String account = null;
@@ -71,7 +70,7 @@ public class Home {
     }
 
     private JComboBox<String> createAccountComboBox() {
-        JComboBox<String> comboBox = new JComboBox<>(new String[]{"Select Account", "115", "218", "537", "903"});
+        JComboBox<String> comboBox = new JComboBox<>(new String[] {"Select Account", "115", "218", "537", "903"});
         comboBox.setBounds(44, 67, 116, 22);
         comboBox.addActionListener(e -> account = (String) comboBox.getSelectedItem());
         return comboBox;
@@ -80,24 +79,16 @@ public class Home {
     private void deleteRecords(String account) {
         String sql;
         switch (account) {
-            case "537":
-                sql = "DELETE FROM Patients";
-                break;
-            case "903":
-                sql = "DELETE FROM OnCall_903";
-                break;
-            case "218":
-                sql = "DELETE FROM Advantage_218";
-                break;
-            case "115":
-                sql = "DELETE FROM Properties_115";
-                break;
-            default:
+            case "537" -> sql = "DELETE FROM Patients";
+            case "903" -> sql = "DELETE FROM OnCall_903";
+            case "218" -> sql = "DELETE FROM Advantage_218";
+            case "115" -> sql = "DELETE FROM Properties_115";
+            default -> {
                 JOptionPane.showMessageDialog(null, "Unsupported account");
                 return;
+            }
         }
-        try (Connection conn = getDatabaseConnection(account);
-             PreparedStatement pst = conn.prepareStatement(sql)) {
+        try (Connection conn = getDatabaseConnection(account); PreparedStatement pst = conn.prepareStatement(sql)) {
             pst.execute();
             JOptionPane.showMessageDialog(null, "Records deleted for account: " + account);
         } catch (SQLException e) {
@@ -107,180 +98,11 @@ public class Home {
     }
 
     private Connection getDatabaseConnection(String account) throws SQLException {
-        String connectionUrl = "jdbc:sqlserver://AmtelcoRDS6.miAmtelcoCloud.com;" +
-                (account.equals("537") ? "database=537_2414_1;" : "database=Customers_2414_1;") +
-                "user=user2414;password=N6sgWPUTcp6J;loginTimeout=30;";
-        return DriverManager.getConnection(connectionUrl);
+        String host = System.getenv("DB_HOST");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+        String dbName = account.equals("537") ? System.getenv("DB_NAME_537") : System.getenv("DB_NAME_CUSTOMERS");
+        String url = String.format("jdbc:sqlserver://%s;database=%s;user=%s;password=%s;loginTimeout=30;", host, dbName, user, password);
+        return DriverManager.getConnection(url);
     }
 }
-=======
-import java.text.ParseException;
-import javax.swing.*;
-
-public class Home {
-
-	private JFrame frmAbsecLlc;
-	public String realm = null;
-	public String account = null;
-//	private final ButtonGroup buttonGroup = new ButtonGroup();
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Home window = new Home();
-					window.frmAbsecLlc.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public Home() throws ParseException {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() throws ParseException {
-		frmAbsecLlc = new JFrame();
-		frmAbsecLlc.setTitle("ANSWER CENTRE OF LA");
-		frmAbsecLlc.setBounds(100, 100, 240, 296);
-		frmAbsecLlc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmAbsecLlc.getContentPane().setLayout(new CardLayout(0, 0));
-
-		final JPanel panelMenu = new JPanel();
-		frmAbsecLlc.getContentPane().add(panelMenu, "name_1473696262299771");
-		panelMenu.setLayout(null);
-		panelMenu.setVisible(true);
-		 
-		 JComboBox comboBox = new JComboBox(); comboBox.setModel(new DefaultComboBoxModel(new String[] {
-		 "Select Account","537", "903" })); comboBox.setToolTipText("Account"); comboBox.setBounds(44,
-		 67, 116, 22); panelMenu.add(comboBox);
-		 comboBox.setSelectedIndex(0);
-		 comboBox.addActionListener(new ActionListener() {
-			 
-			 @Override
-			 public void actionPerformed(ActionEvent e) {
-				 if(e.getSource()==comboBox) {
-					 account = (String) comboBox.getSelectedItem();
-					 
-					 System.out.println("Account is " + account);
-					 
-					 if (account == "537") {txtSelectAccount.setText("Hospital Medicine");}
-					 else if (account == "903") {txtSelectAccount.setText("Chevron");
-					 
-					//	 System.out.println(comboBox.getSelectedIndex());
-					 
-					 
-					 }
-				 
-				 
-			 }
-			 } });
-		  
-		 
-		JButton btnGo = new JButton("IMPORT LIST");
-		btnGo.setBounds(44, 149, 116, 23);
-		panelMenu.add(btnGo);
-		btnGo.addActionListener(new ActionListener() {
-			
-		//	String account="537";
-			
-		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-Import2SQL.main(account);
-				
-				
-JOptionPane.showMessageDialog(panelMenu, "List Imported");
-
-			}
-		});
-
-		JButton btnDelete = new JButton("DELETE ALL");
-		btnDelete.setBounds(44, 106, 116, 23);
-		panelMenu.add(btnDelete);
-		
-		txtSelectAccount = new JTextField();
-		txtSelectAccount.setHorizontalAlignment(SwingConstants.CENTER);
-		txtSelectAccount.setText("SELECT ACCOUNT");
-		txtSelectAccount.setBounds(44, 39, 113, 20);
-		panelMenu.add(txtSelectAccount);
-		txtSelectAccount.setColumns(10);
-		btnDelete.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				if (account == "903") {
-
-				Connection conn;
-				try {
-
-
-					DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-					
-//					String connectionUrl = "jdbc:sqlserver://acla.cnbusmu0fj2t.us-east-1.rds.amazonaws.com;" + "user=sa;" + "password=5020Glendale;" + "loginTimeout=30;" + "database=Customers;";
-					String connectionUrl = "jdbc:sqlserver://AmtelcoRDS6.miAmtelcoCloud.com;" + "database=Customers_2414_1;" + "user=user2414;" + "password=N6sgWPUTcp6J;" + "loginTimeout=30;";
-					conn = DriverManager.getConnection(connectionUrl);
-
-					String delete_all = "DELETE FROM OnCall_903";
-
-					PreparedStatement pst = conn.prepareStatement(delete_all);
-					pst.execute();
-					
-				JOptionPane.showMessageDialog(panelMenu, "Worker List Deleted");
-				
-				} catch (SQLException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-				}
-				else if (account == "537") {
-					
-					Connection conn;
-					try {
-
-
-						DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-						
-
-						String connectionUrl = "jdbc:sqlserver://AmtelcoRDS6.miAmtelcoCloud.com;" + "database=537_2414_1;" + "user=user2414;" + "password=N6sgWPUTcp6J;" + "loginTimeout=30;";
-						conn = DriverManager.getConnection(connectionUrl);
-
-						String delete_all = "DELETE FROM Patients";
-
-						PreparedStatement pst = conn.prepareStatement(delete_all);
-						pst.execute();
-						
-					JOptionPane.showMessageDialog(panelMenu, "Patient List Deleted");
-					
-					} catch (SQLException e2) {
-						// TODO Auto-generated catch block
-						e2.printStackTrace();
-					}
-					
-				};
-				
-			}
-		});
-		;
-
-	}
-	;
-
-	JPanel panel = new JPanel();
-	private JTextField txtSelectAccount;
-}
-
->>>>>>> bb500479f201c5b0b636cd9283c4f25d780a2c0f
